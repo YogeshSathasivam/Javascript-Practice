e5f422e26750d60f54b2720b050982b953d71b73
YogeshSathasivam <yogeshsathasivam2002@gmail.com>
2024-01-29 16:55:24 +0530

    Flames Application

--- Flames Application/script.js
+++ Flames Application/script.js
@@ -7,50 +7,44 @@ function calculateFLAMES() {
     flame(a, b);
 }
 
-function clearFields() {
-    document.getElementById('yourName').value = '';
-    document.getElementById('partnerName').value = '';
-    document.getElementById('result').innerText = '';
-}
-
 function flame(a, b) {
-    let i, j, k, l = 1, n, m, sc = 0, tc, rc = 0, fc = 5;
+    let i, j, k, l = 1, yLength, pLength, sChar = 0, tChar, rChar = 0, lStop = 5;
     let c;
     let f = "flames";
 
-    let q = a.join("");
-    let w = b.join("");
-    n = a.length;
-    m = b.length;
-    tc = n + m;
+    let yName = a.join("");
+    let pName = b.join("");
+    yLength = a.length;
+    pLength = b.length;
+    tChar = yLength + pLength;
 
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < yLength; i++) {
         c = a[i];
-        for (j = 0; j < m; j++) {
+        for (j = 0; j < pLength; j++) {
             if (c == b[j]) {
                 a[i] = -1;
                 b[j] = -1;
-                sc = sc + 2;
+                sChar = sChar + 2;
                 break;
             }
         }
     }
 
-    rc = tc - sc;
+    rChar = tChar - sChar;
 
     for (i = 0; ; i++) {
-        if (l == rc) {
+        if (l == rChar) {
             for (k = i; k < f.length; k++) {
                 f = f.substring(0, k) + f.substring(k + 1);
             }
-            fc = fc - 1;
+            lStop = lStop - 1;
             i = i - 1;
             l = 0;
         }
-        if (i == fc) {
+        if (i == lStop) {
             i = -1;
         }
-        if (fc == 0) {
+        if (lStop == 0) {
             break;
         }
         l++;
@@ -58,15 +52,21 @@ function flame(a, b) {
 
     let resultContainer = document.getElementById('result');
     if (f[0] == 'e')
-        resultContainer.innerText = q + " is ENEMY to " + w;
+        resultContainer.innerText = yName + " is ENEMY to " + pName;
     else if (f[0] == 'f')
-        resultContainer.innerText = q + " is FRIEND to " + w;
+        resultContainer.innerText = yName + " is FRIEND to " + pName;
     else if (f[0] == 'm')
-        resultContainer.innerText = q + " is going to MARRY " + w;
+        resultContainer.innerText = yName + " is going to MARRY " + pName;
     else if (f[0] == 'l')
-        resultContainer.innerText = q + " is in LOVE with " + w;
+        resultContainer.innerText = yName + " is in LOVE with " + pName;
     else if (f[0] == 'a')
-        resultContainer.innerText = q + " has more AFFECTION on " + w;
+        resultContainer.innerText = yName + " has more AFFECTION on " + pName;
     else
-        resultContainer.innerText = q + " and " + w + " are SISTERS/BROTHERS ";
+        resultContainer.innerText = yName + " and " + pName + " are SISTERS/BROTHERS ";
+}
+
+function clearFields() {
+    document.getElementById('yourName').value = '';
+    document.getElementById('partnerName').value = '';
+    document.getElementById('result').innerText = '';
 }
